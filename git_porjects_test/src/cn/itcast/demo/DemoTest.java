944191b81f1615c8b35cdde666d9ed81c061e898
luohao <1377086855@qq.com>
2019-06-12 19:26:59 +0800

    添加工程到git

---
+++ git_porjects_test/src/cn/itcast/demo/DemoTest.java
@@ -0,0 +1,16 @@
+package cn.itcast.demo;
+
+public class DemoTest {
+    public static void main(String[] args) {
+        System.out.println("樊文凯大傻子");
+        System.out.println("樊文凯小傻子");
+        System.out.println("樊文凯大智障");
+        System.out.println("樊文凯小智障");
+
+    }
+
+    public void test(){
+        int i = 10;
+        i += 3;
+    }
+}
